741dbd516c1103f5a6c3650409868311044c24b4
Dineshpalappdeveloper <dineshkr748199@gmail.com>
2024-02-04 15:21:56 +0530

    model serializer

--- gshop_app/api_file/serializers.py
+++ gshop_app/api_file/serializers.py
@@ -11,28 +11,39 @@ def alphanumeric(value):
     return value
 
 
-class CarSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(max_length=255)
-    description = serializers.CharField()
-    is_active = serializers.BooleanField(read_only=True)
-    chassisnumber = serializers.CharField(validators=[alphanumeric])
-    price = serializers.DecimalField(max_digits=9, decimal_places=2)
+class CarSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CarList
+        # all attributes
+        # fields = '__all__'
+        # exclude = ['name'] kisi ek field ko nahi lena ho to
+        exclude = ['name']
+        # if specified,
+        # fields = ('id', 'name', 'description', 'is_active',)
 
-    def create(self, validated_data):
-        return CarList.objects.create(**validated_data)
 
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.description = validated_data.get(
-            'description', instance.description)
-        instance.is_active = validated_data.get(
-            'is_active', instance.is_active)
-        instance.chassisnumber = validated_data.get(
-            'chassisnumber', instance.chassisnumber)
-        instance.price = validated_data.get('price', instance.price)
-        instance.save()
-        return instance
+# class CarSerializer(serializers.Serializer):
+    # id = serializers.IntegerField(read_only=True)
+    # name = serializers.CharField(max_length=255)
+    # description = serializers.CharField()
+    # is_active = serializers.BooleanField(read_only=True)
+    # chassisnumber = serializers.CharField(validators=[alphanumeric])
+    # price = serializers.DecimalField(max_digits=9, decimal_places=2)
+
+    # def create(self, validated_data):
+    #     return CarList.objects.create(**validated_data)
+
+    # def update(self, instance, validated_data):
+    #     instance.name = validated_data.get('name', instance.name)
+    #     instance.description = validated_data.get(
+    #         'description', instance.description)
+    #     instance.is_active = validated_data.get(
+    #         'is_active', instance.is_active)
+    #     instance.chassisnumber = validated_data.get(
+    #         'chassisnumber', instance.chassisnumber)
+    #     instance.price = validated_data.get('price', instance.price)
+    #     instance.save()
+    #     return instance
 
     # field labels validation
 
