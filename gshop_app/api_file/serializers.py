628b4250354de9fc912080841ea5000268efa5a4
Dineshpalappdeveloper <dineshkr748199@gmail.com>
2024-02-04 14:59:02 +0530

    validation added

--- gshop_app/api_file/serializers.py
+++ gshop_app/api_file/serializers.py
@@ -1,12 +1,23 @@
 from rest_framework import serializers
 from ..models import CarList
 
+# validators
+
+
+def alphanumeric(value):
+    if not value.isalnum():
+        raise serializers.ValidationError(
+            {'chassisnumber': 'Chassis number must be alphanumeric'})
+    return value
+
 
 class CarSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField(max_length=255)
     description = serializers.CharField()
     is_active = serializers.BooleanField(read_only=True)
+    chassisnumber = serializers.CharField(validators=[alphanumeric])
+    price = serializers.DecimalField(max_digits=9, decimal_places=2)
 
     def create(self, validated_data):
         return CarList.objects.create(**validated_data)
@@ -17,5 +28,24 @@ class CarSerializer(serializers.Serializer):
             'description', instance.description)
         instance.is_active = validated_data.get(
             'is_active', instance.is_active)
+        instance.chassisnumber = validated_data.get(
+            'chassisnumber', instance.chassisnumber)
+        instance.price = validated_data.get('price', instance.price)
         instance.save()
         return instance
+
+    # field labels validation
+
+    def validate_price(self, value):
+        if value <= 20000.00:
+            raise serializers.ValidationError(
+                'Price must be greater than 20000.00')
+        return value
+
+    # object validation
+
+    def validate(self, data):
+        if data['name'] == data['description']:
+            raise serializers.ValidationError(
+                {'name': 'Name and description must be different'})
+        return data
--- gshop_app/models.py
+++ gshop_app/models.py
@@ -7,3 +7,5 @@ class CarList(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=100)
     is_active = models.BooleanField(default=False)
+    chassisnumber = models.CharField(max_length=100, blank=True, null=True)
+    price = models.DecimalField(max_digits=9, decimal_places=2, null=True)
